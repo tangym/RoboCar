a2f71fb5d017c7d03d8226d911c1345ea1dd3dbd
tangym <yut44@psu.edu>
2022-11-20 02:00:38 +0000

    Update run commands

--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,14 @@
-python3 /home/pi/RoboCar/src/message_client/client.py &
+# Run GPIO deamon
+if pgrep -x "pigpiod" > /dev/null
+then
+    sudo pigpiod
+fi
+
+# Run 5G driver 
+if pgrep -x "simcom-cm" > /dev/null
+then
+    sudo /home/pi/SIM8200_for_RPI/Goonline/simcom-cm &
+fi
+
+# Run RoboCar
+python3 /home/pi/RoboCar/src/message_client/client.py | sudo /home/pi/RoboCar/src/car_movement/hw0a1blikey2
