99e33d862818a4dc8e3ac8e5962cf39a18e1a0b6
tangym <yut44@psu.edu>
2022-12-02 21:20:58 +0000

    Update config

--- src/message_client/config.py
+++ src/message_client/config.py
@@ -1,4 +1,10 @@
-debug = False
+import os
+
+if "raspberry" not in os.uname().version:
+    debug = True
+else:
+    debug = False
+
 cseBaseName = "acme-onem2m-cse-ix62mzt6pa-uc.a.run.app"    # <== Put your own ACME CSE server here
 use_https = True
 encoding = "ISO-8859-1"
@@ -11,11 +17,11 @@ shellOutputContainer = "shellOutput"
 originator = "CAdmin" # "RoboCar" 
 releaseVersionIndicator = "3"
 aeBaseName = "/cse-in/CAdmin"
-logFile = "RoboCar.log" # "/home/pi/RoboCar.log"
-if debug:
-    retrieveDelay = 3
-else:
-    retrieveDelay = 0.25
+
+logFile = "/home/pi/RoboCar.log" if not debug else "RoboCar.log"
+retrieveDelay = 0.25 if not debug else 3
+
 trafficLightPin = 10
 stopSignPin = 11
-motorControlProgram = "/home/pi/RoboCar/src/car_movement/hw0a1blikey2"  # or "python dummy_movement.py"
+
+motorControlProgram = "/home/pi/RoboCar/src/car_movement/hw0a1blikey2" if not debug else "python dummy_movement.py"
